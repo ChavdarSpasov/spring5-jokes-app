3e1fd46c6bab063b6ba1ab686a5d468d02d19e57
ChavdarSpasov <chspasov@abv.bg>
2026-08-03 14:27:22 +0300

    implement joke service and controllers . close #3

--- src/main/java/mytest/springframework/spring5_jokes_app/controllers/JokeController.java
+++ src/main/java/mytest/springframework/spring5_jokes_app/controllers/JokeController.java
@@ -1,11 +1,11 @@
 package mytest.springframework.spring5_jokes_app.controllers;
 
 import mytest.springframework.spring5_jokes_app.services.JokeService;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Service
+@Controller
 public class JokeController {
     private final JokeService jokeService;
 
@@ -13,10 +13,29 @@ public class JokeController {
         this.jokeService = jokeService;
     }
 
-
-    @RequestMapping({"/", ""})
+    @RequestMapping({"/", " "})
     public String showJoke(Model model) {
         model.addAttribute("joke", jokeService.getJoke());
         return "index";
+    }
+
+    @RequestMapping({"/secondJoke"})
+    public String showJokeTwo(Model model) {
+        model.addAttribute("secondJoke", jokeService.getJoke());
+        return "secondJoke";
+    }
+
+    @RequestMapping({"/thirdJoke"})
+    public String showJokeThree(Model model) {
+        model.addAttribute("thirdJoke", jokeService.getJoke());
+        return "thirdJoke";
+    }
+
+    @RequestMapping({"/allJokes"})
+    public String showAllJokes(Model model) {
+        model.addAttribute("joke1", jokeService.getJoke());
+        model.addAttribute("joke2", jokeService.getJoke());
+        model.addAttribute("joke3", jokeService.getJoke());
+        return "allJokes";
     }
 }
